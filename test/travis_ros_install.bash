5d39074bbc4139a074f279b3e4ff53e730209863
nakajob <35713978+nakajob@users.noreply.github.com>
2018-03-05 16:38:34 +0900

    Update travis_ros_install.bash

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -8,7 +8,7 @@ pip install rospkg
 
 #ros install
 cd ..
-git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+git clone https://github.com/nakajob/ros_setup_scripts_Ubuntu14.04_server.git
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
@@ -16,7 +16,7 @@ bash ./step1.bash
 #catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-source /opt/ros/indigo/setup.bash
+source /opt/ros/kinetic/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
